9e38072f2b4be2318e12f1395dc23632bd3e4b6e
henadzit <henadzi.tsaryk@gmail.com>
2025-02-10 12:54:03 +0100

    Remove benchmarks with a single iteration

--- tests/benchmarks/test_dumper.py
+++ tests/benchmarks/test_dumper.py
@@ -7,16 +7,6 @@ from psycopg._enums import PyFormat
 PSYCOPG_TEST_DSN = "host=localhost dbname=psycopg_test user=postgres password=123456"
 
 
-def test_uuid_text_dumper(benchmark, conn):
-    val = UUID("12345678123456781234567812345679")
-    tx = psycopg.adapt.Transformer(conn)
-
-    @benchmark
-    def bench():
-        d = tx.get_dumper(val, PyFormat.TEXT)
-        d.dump(val)
-
-
 def test_uuid_text_dumper_100(benchmark, conn):
     val = UUID("12345678123456781234567812345679")
     tx = psycopg.adapt.Transformer(conn)
@@ -26,16 +16,6 @@ def test_uuid_text_dumper_100(benchmark, conn):
         d = tx.get_dumper(val, PyFormat.TEXT)
         for _ in range(100):
             d.dump(val)
-
-
-def test_uuid_binary_dumper(benchmark, conn):
-    val = UUID("12345678123456781234567812345679")
-    tx = psycopg.adapt.Transformer(conn)
-
-    @benchmark
-    def bench():
-        d = tx.get_dumper(val, PyFormat.BINARY)
-        d.dump(val)
 
 
 def test_uuid_binary_dumper_100(benchmark, conn):
