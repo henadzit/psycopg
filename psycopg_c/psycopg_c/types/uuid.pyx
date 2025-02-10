a667ceb07007f0e29de4b46d1b45eb5d0f20f36f
henadzit <henadzi.tsaryk@gmail.com>
2025-02-10 23:37:50 +0100

    Remove C UUIDDumper and UUIDBinaryDumper

--- psycopg_c/psycopg_c/types/uuid.pyx
+++ psycopg_c/psycopg_c/types/uuid.pyx
@@ -1,15 +1,8 @@
 cimport cython
 
-from types import ModuleType
-from cpython.bytes cimport PyBytes_AsString
 from cpython.long cimport PyLong_FromUnsignedLongLong
 
-cdef extern from "Python.h":
-    # PyUnicode_AsUTF8 was added to cpython.unicode in 3.1.x but we still
-    # support 3.0.x
-    const char *PyUnicode_AsUTF8(object unicode) except NULL
-
-cdef extern from * nogil:
+cdef extern from *:
     """
 #if BYTE_ORDER == LITTLE_ENDIAN
 #    define UINT128_HOST_HIGH_IDX 1
@@ -18,37 +11,7 @@ cdef extern from * nogil:
 #    define UINT128_HOST_HIGH_IDX 0
 #    define UINT128_HOST_LOW_IDX 1
 #endif
-    """
-    cdef size_t UINT128_HOST_HIGH_IDX
-    cdef size_t UINT128_HOST_LOW_IDX
 
-
-@cython.final
-cdef class UUIDDumper(CDumper):
-    format = PQ_TEXT
-    oid = oids.UUID_OID
-
-    cdef Py_ssize_t cdump(self, obj, bytearray rv, Py_ssize_t offset) except -1:
-        cdef const char *src = PyUnicode_AsUTF8(obj.hex)
-        cdef char *buf = CDumper.ensure_size(rv, offset, 32)
-        memcpy(buf, src, 32)
-        return 32
-
-
-@cython.final
-cdef class UUIDBinaryDumper(CDumper):
-    format = PQ_BINARY
-    oid = oids.UUID_OID
-
-    cdef Py_ssize_t cdump(self, obj, bytearray rv, Py_ssize_t offset) except -1:
-        cdef const char *src = PyBytes_AsString(obj.bytes)
-        cdef char *buf = CDumper.ensure_size(rv, offset, 16)
-        memcpy(buf, src, 16)
-        return 16
-
-
-cdef extern from *:
-    """
 static const int8_t hex_to_int_map[] = {
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 0-15
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 16-31
@@ -67,7 +30,9 @@ static const int8_t hex_to_int_map[] = {
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  // 224-239
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1   // 240-255
 };
-"""
+    """
+    cdef size_t UINT128_HOST_HIGH_IDX
+    cdef size_t UINT128_HOST_LOW_IDX
     const int8_t[256] hex_to_int_map
 
 
